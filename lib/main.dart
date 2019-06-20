c3950728b51144d6aa2350c6eb07cb53d9c93083
Naing Myo Thaw <naingmyothaw.nmyt@gmail.com>
2019-06-20 22:04:39 +0630

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_font/home.dart';
+
+void main() => runApp(CustomFontApp());
+
+class CustomFontApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Font',
+      home: Home(),
+    );
+  }
+}
